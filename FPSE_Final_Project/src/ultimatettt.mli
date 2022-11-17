4da9c088a64b7aa181153a84e2b103bcd54ab2bd
Dakh121 <77988764+Dakh121@users.noreply.github.com>
2022-11-16 20:22:09 -0500

    adding additional functions and sorting them

--- FPSE_Final_Project/src/ultimatettt.mli
+++ FPSE_Final_Project/src/ultimatettt.mli
@@ -1,4 +1,4 @@
-
+(* –––––– TYPES –––––– *)
 (* Represents an individual square in a tic-tac-toe board *)
 type square = 
   | X
@@ -14,12 +14,14 @@ type square =
    *)
 type board = square list
 
-type ttt_board = board list
+type ult_board = board list
 
 (* Represents the current player in the game *)
 type player = 
   | X 
   | O
+
+(* –––––– GENERAL FUNCTIONS –––––– *)
 
 (* Switches the current player (from X to O or from O to X) *)
 val switch_player : player -> player 
@@ -29,8 +31,47 @@ val switch_player : player -> player
    *)
 val place_mark : board -> player -> (int*int) -> board
 
+(* Returns the ttt board that the next player must place a X or O within *)
+val next_board_move : ult_board -> board
+
+
+(* –––––– WIN CONDITION CHECKS –––––– *)
+
 (* Checks if either X or O has won yet, returns player x if X, 
   player o if O, and None if no one has won yet
   *)
-val check_win : board -> player option
+val check_win_board : board -> player option
 
+(* checks the verticals for a given board; 
+   returns Some player if they won from a diagonal
+    *)
+val check_verticals : board -> player option
+
+(* checks the horizonals for a given board; 
+   returns Some player if they won from a diagonal
+    *)
+val check_horizontals : board -> player option
+
+(* checks the diagonals for a given board; 
+   returns Some player if they won from a diagonal
+    *)
+val check_diagonals : board -> player option
+
+(* checks if a player has won the entire ultimate ttt game;
+   returns Some player if they won
+  *)
+val check_win_complete : ult_board -> player option
+
+(* Checks if a given move is valid; true if valid, false if not *)
+val check_valid : board -> (int*int) -> player -> bool
+
+
+(* ––––––  BOARD/GAME MANIPULATION ––––––*)
+
+(* Completely resets the board *)
+val reset : ult_board -> ult_board
+
+(* A player decides to forfeit the game; announce the player that won,  
+   and clear the game
+  *)
+val forfeit : player -> ult_board -> player
